9dc0da7c7deabc34bcbc358744957117efdb2ad8
Luis Alves <luisfernandorsalves@gmail.com>
2021-05-31 20:13:58 -0600

    setting up eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from "react"
+import Component from "../app/Components/test"
 
 export interface AppProps {
   arg: string;
 }
 
-export const App = ({arg}: AppProps) => (
+export const App = ({arg}: AppProps):JSX.Element => (
   <h1>
     This is my {arg}
+    <Component/>
   </h1>
-);
+)
