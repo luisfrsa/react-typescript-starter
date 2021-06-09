482a0b665fc3663ab93497cfd16dc2a8468f433c
Luis Alves <luisfernandorsalves@gmail.com>
2021-06-08 18:22:34 -0600

    Setting up prettier and enhancing eslint

--- src/Components/Component.tsx
+++ src/Components/Component.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React from 'react'
 
 const Component = (): JSX.Element => {
-    return (<div>Hello</div>)
+    return <div>Hello</div>
 }
 export default Component
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
-import React from "react"
-import Component from "../Components/Component"
+import React from 'react'
+import Component from '../Components/Component'
 export interface AppProps {
-  arg: string;
+    arg: string
 }
 
-export const App = ({arg}: AppProps):JSX.Element => (
-  <h1>
-    This is my {arg}
-     <Component/>
-  </h1>
+export const App = ({ arg }: AppProps): JSX.Element => (
+    <h1>
+        This is my {arg}
+        <Component />
+    </h1>
 )
